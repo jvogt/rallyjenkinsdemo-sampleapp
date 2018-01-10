10500bb7f5855b24fc2d9e130bed9fa10efc2324
Jeff Vogt <jvogt@chef.io>
2018-01-10 03:08:34 -0800

    revert

---
+++ get_container_ip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+  echo "Must pass container name as argument"
+  exit 1
+fi
+echo $("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${1}")
