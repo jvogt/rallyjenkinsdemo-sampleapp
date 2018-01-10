de2a3c1e15f7790a254f109a0610e89c92462c13
Jeff Vogt <jvogt@chef.io>
2018-01-10 03:19:13 -0800

    rename

--- get_acceptance_container_ip.sh
+++ get_acceptance_container_ip.sh
@@ -3,4 +3,4 @@ if [ "$1" == "" ]; then
   echo "Must pass container name as argument"
   exit 1
 fi
-echo $("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${1}")
+echo $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${1}")
