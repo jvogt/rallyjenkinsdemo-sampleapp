48acbcbca36229fd7e5f2720e763f6a515860b05
Jeff Vogt <jvogt@chef.io>
2018-01-10 17:21:05 -0800

    bugfix and remove test

--- start_acceptance_container.sh
+++ start_acceptance_container.sh
@@ -6,6 +6,6 @@ fi
 
 echo "Starting acceptance container"
 docker kill rallyjenkinsdemo_app_acceptance || true
-docker run --network=rallyjenkinsdemo_default -d $TARGET_DOCKER_IMAGE --name="${1}"
+docker run --network=rallyjenkinsdemo_default --name="${1}" -d $TARGET_DOCKER_IMAGE
 echo "Waiting for Jetty to stand up"
 sleep 20
